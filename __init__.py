c2ba93678f651fb8185d37868c19b0a64f83377e
TREE Industries <30479526+TREE-Ind@users.noreply.github.com>
2021-06-08 21:08:59 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,31 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.messagebus import Message
+from textblob import TextBlob
 
 
 class SentimentAnalysis(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('analysis.sentiment.intent')
-    def handle_analysis_sentiment(self, message):
-        self.speak_dialog('analysis.sentiment')
+    def initialize(self):
+        self.add_event('recognizer_loop:utterance', self.handle_sentiment)
+        
+        self.add_event('speak', self.handle_ai_sentiment)
+    
+    def handle_sentiment(self, message):
+        senti = str(message.data.get('utterances'))
+        #senti = message.data.get('utterance')
+        senti = TextBlob(senti)
+        senti = senti.sentiment.polarity
+        print(senti)
+        self.bus.emit(Message("sentiment",{"data": senti} ))
+        
+    def handle_ai_sentiment(self, message):
+        senti = str(message.data.get('utterance'))
+        senti = TextBlob(senti)
+        senti = senti.sentiment.polarity
+        self.bus.emit(Message("sentiment",{"data": senti} ))
+        
 
 
 def create_skill():
